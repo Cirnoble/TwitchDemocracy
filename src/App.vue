9673bfeb41a6571695874198160acbcfe3f038db
Cirnoble <stencilost@gmail.com>
2025-07-06 14:37:14 -0500

    styling

--- src/App.vue
+++ src/App.vue
@@ -11,42 +11,16 @@ onMounted(()=>{
 })
 </script>
 
-<template>
-  <main >
-    <AccountInfo></AccountInfo>
-    <Chat></Chat>
-  </main>
+<template class="container">
+      <AccountInfo></AccountInfo>
+      <Chat></Chat>
 </template>
 
 <style scoped>
-main{
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr auto;
-}
-header {
-  line-height: 1.5;
+.container{
+  margin: 0 auto;
+  min-width: 500px;
+  border: 1px solid blue;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/components/AccountInfo.vue
+++ src/components/AccountInfo.vue
@@ -1,13 +1,17 @@
 <template>
-    <div v-if="useTwitchAuth().isAuthenticated()" class="header">
-        <img
+    <div >
+        <div v-if="useTwitchAuth().isAuthenticated()" class="header">
+            <img
             :src="useTwitchAuth().getUser()?.profile_image_url ?? ''"
-        />
-        <h1>Username: {{ useTwitchAuth().getUser()?.display_name ?? "Error"}}</h1>
-        <button @click="onLogout"> Logout</button>
-    </div>
-    <div v-else>
-        <a :href="useTwitchAuth().getUserAuthUrl()">Connect to Twitch</a>
+            />
+            <h1>Username: {{ useTwitchAuth().getUser()?.display_name ?? "Error"}}</h1>
+            <button @click="onLogout"> Logout</button>
+        </div>
+        <div v-else class="header">
+            <span> </span>
+            <a :href="useTwitchAuth().getUserAuthUrl()">Connect to Twitch</a>
+            <span> </span>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -20,14 +24,32 @@
 </script>
 <style lang="css" scoped>
 .header{
-    grid-template-columns: 1fr 5fr 1fr;
-    max-height: 5rem;
     display: grid;
-    border: 1px solid black
+    grid-template-columns: 1fr 5fr 1fr;
+    height: 5rem;
+    background-color: grey;
+    border-color: black;
+    border-style: solid;
+    border-radius: 0px 0px 10px 10px;
+    border-width: 0 0 2px 0;
+    box-sizing: content-box;
 }
+
 .header > *{
     display: inline-block;
 }
+
+.header > a{
+    margin: auto;
+    padding: 1rem;
+    border: 1px solid;
+    border-radius: 10px;
+    font: bold 2rem black;
+}
+.header > a:hover{
+    border-color: black;
+}
+
 img{
     height: 5rem;
 }
